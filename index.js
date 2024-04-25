d168219dd035d2d2009cb02ebf069b1fae2eab90
Mark Ye <fengy@finfare.com>
2024-04-25 12:55:59 -0700

    esm resolver fix

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import { version, sum, typedSum } from './modules/common/index';
-import { fromModule } from './modules/common/module';
+import { version as verCommon, sum, typedSum } from './modules/common/index.js';
+import { fromModule } from './modules/common/module.js';
 
 export const result = typedSum(1, 2);
 
 export const version = '0.0.4';
 
-export { fromModule } from './modules/common/module';
-export { typedSum } from './modules/common';
+export { fromModule, typedSum };
