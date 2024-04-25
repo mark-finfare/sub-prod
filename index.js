ee034e230490b820b91016ee91436a431352f493
Mark Ye <fengy@finfare.com>
2024-04-25 11:15:47 -0700

    export more stuff

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { fromModule } from './modules/common/module';
 
 export const result = typedSum(1, 2);
 
-export const version = '0.0.3';
+export const version = '0.0.4';
 
 export { fromModule } from './modules/common/module';
+export { typedSum } from './modules/common';
