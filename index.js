0167ddd3304ce9ba4a58fc8e2344b06349b5ab1e
Mark Ye <fengy@finfare.com>
2024-04-24 17:53:43 -0700

    version bump for init package

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { version, sum, typedSum } from './modules/common/index';
+import { fromModule } from './modules/common/module';
 
 export const result = typedSum(1, 2);
 
-export const version = '0.0.0';
+export const version = '0.0.1';
+
+export { fromModule } from './modules/common/module';
